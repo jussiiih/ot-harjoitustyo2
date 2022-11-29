2f831110ca1935c43b5155760b3269906f3a30a9
Heino Juha-Pekka <juheino@vdi-cubic-006.ad.helsinki.fi>
2022-11-29 08:48:38 +0200

    tehtävä8  maksukortti-kohdassa

--- laskarit/viikko2/unicafe/src/tests/kassapaate_test.py
+++ laskarit/viikko2/unicafe/src/tests/kassapaate_test.py
@@ -1,9 +1,11 @@
 import unittest
 from kassapaate import Kassapaate
+from maksukortti import Maksukortti
 
 class TestKassapaate(unittest.TestCase):
     def setUp(self):
         self.kassapaate = Kassapaate()
+        self.maksukortti = Maksukortti(1000)
 
     def test_alussa_kassassa_oikea_maara_rahaa(self):
         self.assertEqual(self.kassapaate.kassassa_rahaa, 100000)
@@ -36,8 +38,36 @@ class TestKassapaate(unittest.TestCase):
         self.kassapaate.syo_maukkaasti_kateisella(500)
         self.assertEqual(self.kassapaate.maukkaat, 1)
 
-    #Jos maksu ei ole riittävä: kassassa oleva rahamäärä ei muutu,
-    #kaikki rahat palautetaan vaihtorahana ja myytyjen lounaiden määrässä ei muutosta
+    def edullisen_maksu_ei_riita_kassassa_raha_ei_muutu(self):
+        self.kassapaate.syo_edullisesti_kateisella(200)
+        self.assertEqual(self.kassapaate.kassassa_rahaa, 100000)
 
+    def edullisen_maksu_ei_riita_maksu_palautetaan(self):
+        self.assertEqual(self.kassapaate.syo_edullisesti_kateisella(200), 200)
+
+    def edullisen_maksu_ei_riita_lounaiden_maara_ei_muutu(self):
+        self.kassapaate.syo_edullisesti_kateisella(200)
+        self.assertEqual(self.kassapaate.edulliset, 0)
+
+    def maukkaan_maksu_ei_riita_kassassa_raha_ei_muutu(self):
+        self.kassapaate.syo_maukkaasti_kateisella(300)
+        self.assertEqual(self.kassapaate.kassassa_rahaa, 100000)
+
+    def maukkaan_maksu_ei_riita_maksu_palautetaan(self):
+        self.assertEqual(self.kassapaate.syo_maukkaasti_kateisella(300), 300)
+
+    def maukkaan_maksu_ei_riita_lounaiden_maara_ei_muutu(self):
+        self.kassapaate.syo_maukkaasti_kateisella(300)
+        self.assertEqual(self.kassapaate.maukkaat, 0)
+
+ #seuraavissa testeissä tarvitaan myös Maksukorttia jonka oletetaan toimivan oikein
+#Korttiosto toimii sekä edullisten että maukkaiden lounaiden osalta
+
+ #   Jos kortilla on tarpeeksi rahaa, veloitetaan summa kortilta ja palautetaan True
+  #  Jos kortilla on tarpeeksi rahaa, myytyjen lounaiden määrä kasvaa
+   # Jos kortilla ei ole tarpeeksi rahaa, kortin rahamäärä ei muutu, myytyjen lounaiden määrä muuttumaton ja palautetaan False
+    #Kassassa oleva rahamäärä ei muutu kortilla ostettaessa
+
+#Kortille rahaa ladattaessa kortin saldo muuttuu ja kassassa oleva rahamäärä kasvaa ladatulla summalla
     
 
