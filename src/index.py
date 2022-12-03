692df07c5d38f05d08145ee7de414a2a771a7758
Heino Juha-Pekka <juheino@vdi-cubic-007.ad.helsinki.fi>
2022-12-03 21:29:15 +0200

    indexiä tehty lisää

--- src/index.py
+++ src/index.py
@@ -6,24 +6,29 @@ class GameStart:
 
     def start(self):
         self.guide()
+        self.input_player()
+        self.prepare.shuffle_players()
 
+
+    def input_player(self):
         while True:
-            player_name = input("Kirjoita pelaajan nimi")
-            
+            player_name = input("Kirjoita pelaajan nimi: ")
+
             if player_name == "valmis":
                 if len(self.prepare.players) < 3:
                     print('Pelaajia täytyy olla vähintään 3')
                     continue
                 else:
-                    self.prepare.shuffle_players
                     break
-
-            if len(player) < 2:
-                print("Nimessä täytyy olla vähintään 2 kirjainta!")
+            
+            if len(player_name) < 2:
+                print('Nimessä täytyy olla vähintään 2 kirjainta!')
                 continue
             
-            forbidden_characters = ""
-            
+            forbidden_charactes = "§1234567890!+´½!#¤%&/()=?`@£${[]}\¨'^*~<>|/-,.-;:_"
+            if any(character in forbidden_charactes for character in player_name):
+                print('Nimessä saa olla vain kirjaimia ja välilyöntejä!')
+
             if player_name in self.prepare.players:
                 print("Nimi on jo käytössä!")
                 continue
@@ -34,3 +39,6 @@ class GameStart:
     def guide (self):
         print("Kirjoita pelaajan nimi, ja paina ENTER.")
         print("Kun kaikki pelaajat on kirjattu, kirjoita valmis")
+
+application = GameStart()
+application.start()
